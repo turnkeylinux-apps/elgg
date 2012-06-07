bf63098a13eb6af5bf438c3940fd73b0c2d33e31
Alon Swartz <alon@turnkeylinux.org>
2012-06-07 13:37:02 +0300

    inithook to set Elgg admin password, email and domain to serve

---
+++ overlay/usr/lib/inithooks/bin/elgg.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+"""Set Elgg admin password, email and domain to serve
+
+Option:
+    --pass=     unless provided, will ask interactively
+    --email=    unless provided, will ask interactively
+    --domain=   unless provided, will ask interactively
+                DEFAULT=www.example.com
+"""
+
+import sys
+import getopt
+import hashlib
+import random
+import string
+
+from dialog_wrapper import Dialog
+from mysqlconf import MySQL
+
+def usage(s=None):
+    if s:
+        print >> sys.stderr, "Error:", s
+    print >> sys.stderr, "Syntax: %s [options]" % sys.argv[0]
+    print >> sys.stderr, __doc__
+    sys.exit(1)
+
+DEFAULT_DOMAIN="www.example.com"
+
+def main():
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
+                                       ['help', 'pass=', 'email=', 'domain='])
+    except getopt.GetoptError, e:
+        usage(e)
+
+    email = ""
+    domain = ""
+    password = ""
+    for opt, val in opts:
+        if opt in ('-h', '--help'):
+            usage()
+        elif opt == '--pass':
+            password = val
+        elif opt == '--email':
+            email = val
+        elif opt == '--domain':
+            domain = val
+
+    if not password:
+        d = Dialog('TurnKey Linux - First boot configuration')
+        password = d.get_password(
+            "Elgg Password",
+            "Enter new password for the Elgg 'admin' account.")
+
+    if not email:
+        if 'd' not in locals():
+            d = Dialog('TurnKey Linux - First boot configuration')
+
+        email = d.get_email(
+            "Elgg Email",
+            "Enter email address for the Elgg 'admin' account.",
+            "admin@example.com")
+
+    if not domain:
+        if 'd' not in locals():
+            d = Dialog('TurnKey Linux - First boot configuration')
+
+        domain = d.get_input(
+            "Elgg Domain",
+            "Enter the domain to serve Elgg.",
+            DEFAULT_DOMAIN)
+
+    if domain == "DEFAULT":
+        domain = DEFAULT_DOMAIN
+
+    domain = domain.strip("/")
+    if not domain.startswith("http://"):
+        domain = "http://%s/" % domain
+
+    salt = "".join(random.choice(string.letters) for line in range(8))
+    hashpass = hashlib.md5(password + salt).hexdigest()
+
+    m = MySQL()
+    m.execute('UPDATE elgg.elgg_users_entity SET salt=\"%s\" WHERE username=\"admin\";' % salt)
+    m.execute('UPDATE elgg.elgg_users_entity SET password=\"%s\" WHERE username=\"admin\";' % hashpass)
+    m.execute('UPDATE elgg.elgg_users_entity SET email=\"%s\" WHERE username=\"admin\";' % email)
+
+    m.execute('UPDATE elgg.elgg_metastrings SET string=\"%s\" WHERE string LIKE \"%%@%%\";' % email)
+    m.execute('UPDATE elgg.elgg_sites_entity SET url=\"%s\" WHERE url LIKE \"http://%%\";' % domain)
+
+if __name__ == "__main__":
+    main()
+
