8c8f1f7725c8038bbb848d314b4c4b29c0a4b192
Jeremy Davis <jeremy@turnkeylinux.org>
2024-02-27 08:33:35 +1100

    Final tidy up (shellcheck, pyright, pycodestyle)

--- overlay/usr/lib/inithooks/bin/elgg.py
+++ overlay/usr/lib/inithooks/bin/elgg.py
@@ -10,24 +10,28 @@ Option:
 
 import sys
 import getopt
-from libinithooks import inithooks_cache
-
 import bcrypt
+import subprocess
+from typing import Optional, NoReturn
 
+from libinithooks import inithooks_cache
 from libinithooks.dialog_wrapper import Dialog
 from mysqlconf import MySQL
-import subprocess
 
-def usage(s=None):
+
+DEFAULT_DOMAIN = "www.example.com"
+
+
+def usage(s: Optional[str | getopt.GetoptError] = None) -> NoReturn:
     if s:
         print("Error:", s, file=sys.stderr)
     print("Syntax: %s [options]" % sys.argv[0], file=sys.stderr)
     print(__doc__, file=sys.stderr)
     sys.exit(1)
 
-DEFAULT_DOMAIN="www.example.com"
 
-def main():
+def main() -> None:
+    opts = ''
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
                                        ['help', 'pass=', 'email=', 'domain='])
@@ -47,7 +51,7 @@ def main():
         elif opt == '--domain':
             domain = val
 
-    if not password:
+    while not password:
         d = Dialog('TurnKey Linux - First boot configuration')
         password = d.get_password(
             "Elgg Password",
@@ -57,20 +61,21 @@ def main():
         if 'd' not in locals():
             d = Dialog('TurnKey Linux - First boot configuration')
 
-        email = d.get_email(
+        email = d.get_email(  # TODO d is poosibly unbound
             "Elgg Email",
             "Enter email address for the Elgg 'admin' account.",
             "admin@example.com")
 
     inithooks_cache.write('APP_EMAIL', email)
 
-    if not domain:
+    while not domain:
         if 'd' not in locals():
             d = Dialog('TurnKey Linux - First boot configuration')
 
-        domain = d.get_input(
+        domain = d.get_input(  # TODO d is poosibly unbound
             "Elgg Domain",
-            "Enter the domain to serve Elgg. Note: Elgg does not support http without further configuration, domain will default to https.",
+            "Enter the domain to serve Elgg. Note: Elgg does not support http"
+            " without further configuration, domain will default to https.",
             DEFAULT_DOMAIN)
 
     if domain == "DEFAULT":
@@ -87,42 +92,52 @@ def main():
     inithooks_cache.write('APP_DOMAIN', domain)
 
     subprocess.run(["sed", "-i",
-                    f'\|^$CONFIG->wwwroot|s|=.*|= "https://{domain}/";|',
+                    f'/^$CONFIG->wwwroot/ s|=.*|= "https://{domain}/";|',
                     '/var/www/elgg/elgg-config/settings.php'])
 
     apache_conf = "/etc/apache2/sites-available/elgg.conf"
+    subprocess.run(
+            ["sed", "-i",
+             f"/RewriteRule/ s|https://.*|https://{domain}/$1 [R=307,L]|",
+             apache_conf
+             ])
     subprocess.run(["sed", "-i",
-                    f"\|RewriteRule|s|https://.*|https://{domain}/\$1 [R,L]|",
-                    apache_conf])
-    subprocess.run(["sed", "-i", f"\|RewriteCond|s|!^.*|!^{domain}$|",
+                    f"/RewriteCond/ s|!^.*|!^{domain}$|",
                     apache_conf])
     subprocess.run(["service", "apache2", "restart"])
 
-    salt = bcrypt.gensalt(10) 
+    salt = bcrypt.gensalt(10)
     hashpass = bcrypt.hashpw(password.encode('utf8'), salt)
 
     m = MySQL()
 
     try:
         with m.connection.cursor() as cursor:
-            cursor.execute('SELECT guid FROM elgg.elgg_entities WHERE type="user" AND owner_guid="0"')
+            cursor.execute('SELECT guid FROM elgg.elgg_entities WHERE'
+                           ' type="user" AND owner_guid="0"')
             admin_guid = cursor.fetchone()['guid']
-            cursor.execute('SELECT name FROM elgg.elgg_metadata WHERE entity_guid=%s', (admin_guid,))
+            cursor.execute('SELECT name FROM elgg.elgg_metadata WHERE'
+                           ' entity_guid=%s',
+                           (admin_guid,))
             assert(cursor.fetchone()['name'])
-            # sanity check, if this fails, look at the database. You'll probably need to update
-            # all of this database stuff
+            # sanity check, if this fails, look at the database. You'll
+            # probably need to update all of this database stuff
             cursor.execute(
-                'UPDATE elgg.elgg_metadata SET value=%s WHERE entity_guid=%s AND name="password_hash"',
+                'UPDATE elgg.elgg_metadata SET value=%s WHERE entity_guid=%s'
+                ' AND name="password_hash"',
                 (hashpass, admin_guid,))
             cursor.execute(
-                'UPDATE elgg.elgg_metadata SET value=%s WHERE entity_guid=%s AND name="email"',
+                'UPDATE elgg.elgg_metadata SET value=%s WHERE entity_guid=%s'
+                ' AND name="email"',
                 (email, admin_guid,))
             cursor.execute(
-                'UPDATE elgg.elgg_metadata SET value=%s WHERE entity_guid=1 AND name="email"',
+                'UPDATE elgg.elgg_metadata SET value=%s WHERE entity_guid=1'
+                ' AND name="email"',
                 (email,))
             m.connection.commit()
     finally:
         m.connection.close()
+
 
 if __name__ == "__main__":
     main()
